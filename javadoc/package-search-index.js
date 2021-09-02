63891d51e2225dfe4714023396395c9d7ee3fed8
kakaxi01 <1564526031@.com>
2021-09-02 14:45:33 +0800

    first commit

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+var packageSearchIndex = [{"l":"com.yaxiu.plugin","url":"com/yaxiu/plugin/package-summary.html"}, {"l":"All packages","url":"index.html"}]
---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+var typeSearchIndex = [{"p":"com.yaxiu.plugin","l":"MainActivity.Companion","url":"com/yaxiu/plugin/MainActivity.Companion.html"}, {"p":"com.yaxiu.plugin","l":"MainActivity","url":"com/yaxiu/plugin/MainActivity.html"}, {"l":"All classes","url":"allclasses.html"}]
